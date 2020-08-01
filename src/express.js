89493b6327a41bcb0063c2eda49be334e7bf9919
Sam Ruby <rubys@intertwingly.net>
2020-08-01 18:41:40 -0400

    forward on authenticated requests

--- src/express.js
+++ src/express.js
@@ -1,10 +1,13 @@
-const express=require('express');
+const express = require('express');
 const fs = require('fs');
+const bodyParser = require('body-parser');
+const http = require('http');
+const https = require('https');
 
-// Configure babel to handle class properties and JSX
+// Configure babel
 require('@babel/register')({
   presets: [
-    ['@babel/preset-env', {targets: {node: true}}]
+    ['@babel/preset-env', { targets: { node: true } }]
   ]
 });
 
@@ -15,9 +18,34 @@ const port = 3001;
 
 let rubyhome = fs.readFileSync('/home/rubys/tmp/rubyhome.ics', 'utf8');
 
-app.post('/', (request, response) => {
-  let jcal = calendar.parse(rubyhome);
-  response.json(calendar.reduce(jcal));
+app.use(bodyParser.json());
+
+app.post('/', async (request, response) => {
+  let { url } = request.body;
+  let agent = url.startsWith('https://') ? https : http;
+  let options = {};
+  if (request.headers.authorization) {
+    options.headers = {Authorization: request.headers.authorization}
+  }
+
+  agent.get(url, options, icalResponse => {
+    const { statusCode, headers } = icalResponse;
+  
+    let body = ''
+    icalResponse.on('data', data => { body += data });
+    icalResponse.on('end', () => {
+      if (statusCode === 200) {
+        let jcal = calendar.parse(body);
+        response.json(calendar.reduce(jcal));
+      } else {
+        response.status(statusCode);
+        if (statusCode === 401 && headers['www-authenticate']) {
+          response.header('www-authenticate', headers['www-authenticate']);
+        }
+        response.send(body);
+      }
+    });
+  });
 });
 
 app.listen(port, () => console.log(`Calendar server listening on port ${port}`));
